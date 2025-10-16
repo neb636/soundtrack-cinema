43b12b6591fb0191548ec62af3363521c1ac0b80
Nicholas Blanchard <nblanchard@eagleinvsys.com>
2025-10-16 17:49:00 -0400

    More initial work

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,6 +6,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home-page.component.css'],
   imports: [CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<div></div>`,
+  template: `
+
+    @if (songSearchResource.value()?.length) {
+    <section class="app-root__results-section">
+    <h2 class="app-root__section-title">Select a Song</h2>
+    <div class="app-root__song-grid">
+    @for (song of songSearchResource.value(); track song.id) {
+    <song-card
+      [song]="song"
+      (click)="applicationStateService.selectSong(song)"
+    ></song-card>
+    }
+    </div>
+    </section>
+    }`,
 })
 export class HomePageComponent {}
---
+++ src/app/pages/song-search-results/song-search-results.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'song-search-results',
+  styleUrls: ['./song-search-results.component.css'],
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: ``,
+})
+export class SongSearchResultsComponent {}
